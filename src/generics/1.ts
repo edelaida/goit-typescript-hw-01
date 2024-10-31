4cbe29a728ba8a54aa9b387aa14b362b2fc1183a
“edelaida” <edelaida@gmail.com>
2024-10-31 17:48:23 +0200

    data axios

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -2,14 +2,12 @@ import axios from 'axios';
 
 async function fetchData<T>(url: string): Promise<T> {
  try {
-  const response: T = await axios.get(url);
+  const response = await axios.get<T>(url);
   return response.data;
  } catch (error) {
    throw new Error(`Error fetching from ${url}: ${error}`);
  }
 }
-
-
 
 
 // Типізуйте асинхронну функцію fetchData, 
